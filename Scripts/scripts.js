188e1f41d61185494d590fbabdd20d7cc0ea58c1
Emmanuel oppong <105021929+pasmanuel@users.noreply.github.com>
2026-02-10 17:15:22 -0700

    new new

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,2 +1,7 @@
 
- 
+  const menuToggle = document.getElementById("menuToggle");
+        const navLinks = document.getElementById("navLinks");
+
+        menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+        });
